99af0a96bc17b5f1983cb6d41ef01d587d3a3211
José Carlos Palma <palmahn@gmail.com>
2021-03-06 19:10:34 -0600

    Formulario reactivo para radios y checkbox.

--- src/app/reactive/switch/switch.component.ts
+++ src/app/reactive/switch/switch.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-switch',
-  templateUrl: './switch.component.html',
-  styleUrls: ['./switch.component.css']
+    selector: 'app-switch',
+    templateUrl: './switch.component.html',
+    styleUrls: ['./switch.component.css']
 })
 export class SwitchComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup = this.fb.group({
+        gender: ['M', Validators.required],
+        notification: [true, Validators.required],
+        theme: ['light', Validators.required],
+        condition: [false, Validators.requiredTrue]
+    });
 
-  ngOnInit(): void {
-  }
+    person = {
+        gender: 'F',
+        notification: true,
+        theme: 'light'
+    };
+
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.form.reset({
+            ...this.person,
+            condition: false
+        });
+
+        this.form.valueChanges.subscribe(({ condition, ...value }) => this.person = { ...value });
+    }
+
+    public submit(): void {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+        console.log(this.form);
+    }
 
 }
