153da72d67450519f490f40b9cf8c2987a052409
José Carlos Palma <palmahn@gmail.com>
2021-03-07 23:49:42 -0600

    Página de registro (formulario inicial).

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup = this.fb.group({
+        name: ['', [Validators.required, Validators.pattern('[A-z\u00C0-\u017F]+ [A-z\u00C0-\u017F]+')]]
+    });
 
-  ngOnInit(): void {
-  }
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit(): void {
+    }
+
+    public isInvalid(fieldName: string): boolean {
+        const field = this.form.get(fieldName) as FormControl;
+        return field?.touched && field?.invalid;
+    }
+
+    public register(): void {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+        console.log(this.form);
+    }
 
 }
