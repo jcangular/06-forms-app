9fd4092ffa12ac6b4fe2b5319e6ab331cd0c96bd
José Carlos Palma <palmahn@gmail.com>
2021-05-02 16:58:00 -0600

    Válidaciones personalizadas para nombre y usuario.

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -8,13 +8,23 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class RegisterComponent implements OnInit {
 
+    private readonly nameRegExp = '[A-z\u00C0-\u017F]+ [A-z\u00C0-\u017F]+';
+    private readonly emailRegExp = '^[a-zA-Z][a-zA-Z0-9_.]+@[a-zA-Z]+(\\.[a-z]{2,3}){1,3}$';
+
     form: FormGroup = this.fb.group({
-        name: ['', [Validators.required, Validators.pattern('[A-z\u00C0-\u017F]+ [A-z\u00C0-\u017F]+')]]
+        name: ['', [Validators.required, Validators.pattern(this.nameRegExp)]],
+        email: ['', [Validators.required, Validators.pattern(this.emailRegExp)]],
+        username: ['', [Validators.required, this.noRootName]]
     });
 
     constructor(private fb: FormBuilder) { }
 
     ngOnInit(): void {
+        this.form.reset({
+            name: 'José Palma',
+            email: 'test1@correo.com',
+            username: 'jcpalma'
+        });
     }
 
     public isInvalid(fieldName: string): boolean {
@@ -28,6 +38,14 @@ export class RegisterComponent implements OnInit {
             return;
         }
         console.log(this.form);
+    }
+
+    public noRootName(control: FormControl): { canNotBeRoot: boolean; } | null {
+        const value: string = control.value?.trim().toLowerCase();
+        if (value === 'root') {
+            return { canNotBeRoot: true };
+        }
+        return null;
     }
 
 }
